b8856424b50f08c3b4a7813b7cb428bc0c56cb80
Daniel Hunter <aefdch@gmail.com>
2017-06-03 00:58:34 -0700

    Added uint8 bit access logic

--- logic/main.c
+++ logic/main.c
@@ -33,6 +33,42 @@ int yscale = 100;
 int channelOffset = 150;
 
 int main () {
+	
+	// UART set up
+	struct termios serial; // Structure to contain UART parameters
+	
+	char* dev_id = "/dev/serial0"; // UART device identifier    
+    uint8_t rxData = 0; // Receive data    
+    int read_bytes = 0;    
+    int HPixel1 = 0;
+    
+    printf("Opening %s\n", dev_id);
+    int fd = open(dev_id, O_RDWR | O_NOCTTY | O_NDELAY);
+    
+    if (fd == -1) { // Open failed
+        perror(dev_id);
+        return -1;
+    }
+
+    // Get UART configuration
+    if (tcgetattr(fd, &serial) < 0) {
+        perror("Getting configuration");
+        return -1;
+    }
+
+    // Set UART parameters in the termios structure
+    serial.c_iflag = 0;
+    serial.c_oflag = 0;
+    serial.c_lflag = 0;
+    serial.c_cflag = BAUDRATE | CS8 | CREAD | PARENB | PARODD;
+    // Speed setting + 8-bit data + Enable RX + Enable Parity + Odd Parity
+
+    serial.c_cc[VMIN] = 0; // 0 for Nonblocking mode
+    serial.c_cc[VTIME] = 0; // 0 for Nonblocking mode
+
+    // Set the parameters by writing the configuration
+    tcsetattr(fd, TCSANOW, &serial); 
+	
 	printf("Welcome to Daniel Hunter's Logic Analyzer!\n");
 	printf("Please enter any desired commands, or enter run to begin.\n");
 	
@@ -41,7 +77,7 @@ int main () {
 	int channel[8][5000];
 	int prevChannelData = 0;
 	char s[3];
-	char inputFromUser[MAX_INPUT];	
+	char inputFromUser[MAX_INPUT];
 	
 	do {
 		fgets(inputFromUser, MAX_INPUT - 1, stdin);
@@ -53,19 +89,39 @@ int main () {
 	wiringPiSetup();
 	pinMode(0, INPUT);
 	
-	for (i = 0; i < 5000; i++) {
-		channel[0][i] = i%2;
-		channel[1][i] = i%2;
-		channel[2][i] = i%2;
-	}
+	//for (i = 0; i < 5000; i++) {
+		//channel[0][i] = i%2;
+		//channel[1][i] = i%2;
+		//channel[2][i] = i%2;
+	//}
 	
-	channel[0][0] = 0;
-	channel[0][1] = 0;
-	channel[0][2] = 0;
-	channel[0][3] = 1;
-	channel[0][4] = 1;
-	channel[0][5] = 1;
-	channel[0][6] = 1;
+	//channel[0][0] = 0;
+	//channel[0][1] = 0;
+	//channel[0][2] = 0;
+	//channel[0][3] = 1;
+	//channel[0][4] = 1;
+	//channel[0][5] = 1;
+	//channel[0][6] = 1;
+	
+	for (i = 0; i < 5000; i++) {
+		// Get a byte
+		do {
+			read_bytes = read(fd, &rxData, 1);
+		} while (read_bytes < 1);
+		
+		// Store the byte as bits
+		channel[0][i] = (rxData & 0x80) ? 1 : 0; // [0] is MSB. so compare to 1000 0000
+		channel[1][i] = (rxData & 0x40) ? 1 : 0;
+		channel[2][i] = (rxData & 0x20) ? 1 : 0;
+		channel[3][i] = (rxData & 0x10) ? 1 : 0;
+		
+		channel[4][i] = (rxData & 0x08) ? 1 : 0;
+		channel[5][i] = (rxData & 0x04) ? 1 : 0;
+		channel[6][i] = (rxData & 0x02) ? 1 : 0;
+		channel[7][i] = (rxData & 0x01) ? 1 : 0;
+		
+		// if channel = trigger cond, break and store current index
+	}
 	
 	while(1) {
 		// Set up the graph
