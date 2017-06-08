d0b5171e71303d9d25a9151e053ad04e2f46ff59
Daniel Hunter <officialdanielhunter@gmail.com>
2017-06-07 17:22:04 -0700

    Fixed Oscope tab size

--- oscilloscope/main.c
+++ oscilloscope/main.c
@@ -27,16 +27,16 @@ const int sineTable[128] =
 	246, 248, 250, 252, 253, 254, 255, 255,
 	255, 255, 255, 254, 253, 252, 250, 248,
 	246, 244, 241, 238, 234, 231, 227, 223,
-	218, 214, 209, 204, 199, 194, 188, 183, 
-	177, 171, 165, 159, 153, 147, 140, 134, 
+	218, 214, 209, 204, 199, 194, 188, 183,
+	177, 171, 165, 159, 153, 147, 140, 134,
 	128, 122, 115, 109, 103,  97,  91,  85,
-	 79,  73,  68,  62,  57,  52,  47,  42,
-	 37,  33,  29,  25,  22,  18,  15,  12,
-	 10,   7,   6,   4,   2,   1,   1,   0,
-	  0,   0,   1,   1,   2,   4,   6,   7,
-	 10,  12,  15,  18,  22,  25,  29,  33,
-	 37,  42,  47,  52,  57,  62,  68,  73,
-	 79,  85,  91,  97, 103, 109, 115, 122
+	79,  73,  68,  62,  57,  52,  47,  42,
+	37,  33,  29,  25,  22,  18,  15,  12,
+	10,   7,   6,   4,   2,   1,   1,   0,
+	0,   0,   1,   1,   2,   4,   6,   7,
+	10,  12,  15,  18,  22,  25,  29,  33,
+	37,  42,  47,  52,  57,  62,  68,  73,
+	79,  85,  91,  97, 103, 109, 115, 122
 };
 
 int detectCommand(char* command);
@@ -54,92 +54,92 @@ int trigger_channel = 1;
 #define BAUDRATE B115200 // UART speed
 
 int main () {
-	
+
 	// UART set up
 	struct termios serial; // Structure to contain UART parameters
-	
-	char* dev_id = "/dev/serial0"; // UART device identifier    
-    uint8_t rxData = 0; // Receive data
-    uint8_t prevRxData = 0;
-    int read_bytes = 0;
-    int i = 0;
-    int HPixel1 = 0;
-    
-    printf("Opening %s\n", dev_id);
-    int fd = open(dev_id, O_RDWR | O_NOCTTY | O_NDELAY);
-    
-    if (fd == -1) { // Open failed
-        perror(dev_id);
-        return -1;
-    }
 
-    // Get UART configuration
-    if (tcgetattr(fd, &serial) < 0) {
-        perror("Getting configuration");
-        return -1;
-    }
+	char* dev_id = "/dev/serial0"; // UART device identifier
+	uint8_t rxData = 0; // Receive data
+	uint8_t prevRxData = 0;
+	int read_bytes = 0;
+	int i = 0;
+	int HPixel1 = 0;
 
-    // Set UART parameters in the termios structure
-    serial.c_iflag = 0;
-    serial.c_oflag = 0;
-    serial.c_lflag = 0;
-    serial.c_cflag = BAUDRATE | CS8 | CREAD | PARENB | PARODD;
-    // Speed setting + 8-bit data + Enable RX + Enable Parity + Odd Parity
+	printf("Opening %s\n", dev_id);
+	int fd = open(dev_id, O_RDWR | O_NOCTTY | O_NDELAY);
 
-    serial.c_cc[VMIN] = 0; // 0 for Nonblocking mode
-    serial.c_cc[VTIME] = 0; // 0 for Nonblocking mode
+	if (fd == -1) { // Open failed
+		perror(dev_id);
+		return -1;
+	}
 
-    // Set the parameters by writing the configuration
-    tcsetattr(fd, TCSANOW, &serial);          
-	
+	// Get UART configuration
+	if (tcgetattr(fd, &serial) < 0) {
+		perror("Getting configuration");
+		return -1;
+	}
+
+	// Set UART parameters in the termios structure
+	serial.c_iflag = 0;
+	serial.c_oflag = 0;
+	serial.c_lflag = 0;
+	serial.c_cflag = BAUDRATE | CS8 | CREAD | PARENB | PARODD;
+	// Speed setting + 8-bit data + Enable RX + Enable Parity + Odd Parity
+
+	serial.c_cc[VMIN] = 0; // 0 for Nonblocking mode
+	serial.c_cc[VTIME] = 0; // 0 for Nonblocking mode
+
+	// Set the parameters by writing the configuration
+	tcsetattr(fd, TCSANOW, &serial);
+
 	// Begin taking user input
 	char s[3];
 	int commandCode = 0;
-	int signalOneOffset = 500;	
+	int signalOneOffset = 500;
 	int channelSelect = 0;
 	char inputFromUser[MAX_INPUT];
-	
+
 	printf("Welcome to Daniel Hunter's O'Scope!\n");
 	printf("Please enter any desired commands, or enter start to begin.\n");
-		
+
 	//for (i = 0; i < 10000; i++) {
-		//do {
-				//read_bytes = read(fd, &rxData, 1);
-		//} while (read_bytes < 1);
-		//printf("%d\n", rxData);
+	//do {
+	//read_bytes = read(fd, &rxData, 1);
+	//} while (read_bytes < 1);
+	//printf("%d\n", rxData);
 	//}
-	
+
 	do {
 		fgets(inputFromUser, MAX_INPUT - 1, stdin);
 		commandCode = detectCommand(inputFromUser);
 	} while (commandCode != 8);
-	
+
 	// Set up graphics
 	int width, height;
 	init(&width, &height);
-	
-	while (1) {	
-		graphSetup(width, height, xscale, yscale, trigger_channel, trigger_slope, trigger_level, mode, nchannels);		
-		for (HPixel1 = 0; HPixel1 < 1920; HPixel1 = HPixel1 + xscale*5) {
-			
+
+	while (1) {
+		graphSetup(width, height, xscale, yscale, trigger_channel, trigger_slope, trigger_level, mode, nchannels);
+		for (HPixel1 = 0; HPixel1 < 1920; HPixel1 = HPixel1 + xscale * 5) {
+
 			// Walk across the screen, reading bytes and drawing data
 			do {
 				read_bytes = read(fd, &rxData, 1);
 			} while (read_bytes < 1);
-			
+
 			// Select which channel to use
 			if (nchannels == 2 && channelSelect || nchannels == 1) {
 				// Draw the first channel
-				Line(HPixel1, prevRxData+signalOneOffset, HPixel1+xscale*5, rxData+signalOneOffset);
+				Line(HPixel1, prevRxData + signalOneOffset, HPixel1 + xscale * 5, rxData + signalOneOffset);
 				channelSelect = 0;
 			} else if (nchannels == 2 && !channelSelect) {
 				// Draw the second channel
-				Line(HPixel1, prevRxData+signalOneOffset, HPixel1+xscale*5, rxData+signalOneOffset);
+				Line(HPixel1, prevRxData + signalOneOffset, HPixel1 + xscale * 5, rxData + signalOneOffset);
 				channelSelect = 1;
 			}
-					
+
 			prevRxData = rxData;
-			
+
 		}
 		End(); // End the picture
 	}
@@ -147,35 +147,35 @@ int main () {
 	fgets(s, 2, stdin); // look at the pic, end with [RETURN]
 	finish(); // Graphics cleanup
 	exit(0);
-	
+
 }
 
 // gcc -I/opt/vc/include -I/opt/vc/include/interface/vmcs_host/linux -I/opt/vc/include/interface/vcos/pthreads main.c -o main -lshapes
 
 graphSetup(width, height, xscale, yscale, trigger_channel, trigger_slope, trigger_level, mode, nchannels) {
-	
+
 	int i;
-	char strToPrint[MAX_INPUT];	
+	char strToPrint[MAX_INPUT];
 
 	Start(width, height); // Start the picture
 	Background(0, 0, 0); // Black background
-	
+
 	Stroke(255, 255, 255, .5);
 	StrokeWidth(2);
-	
+
 	for (i = 0; i < 1920; i = i + 150) {
 		Line(i, 0, i, 1200);
 	}
-	
+
 	for (i = 0; i < 1200; i = i + 150) {
 		Line(0, i, 1920, i);
 	}
-	
+
 	Stroke(255, 255, 255, 1);
 	StrokeWidth(2);
-	Rect(0, 0, 500, 220);	
-	
-	Fill(255, 255, 255, 1); // Set the fill to be fully white for the text	
+	Rect(0, 0, 500, 220);
+
+	Fill(255, 255, 255, 1); // Set the fill to be fully white for the text
 	int textHeight = 10;
 	int textSize = 20;
 	int textOffset = 30;
@@ -193,22 +193,22 @@ graphSetup(width, height, xscale, yscale, trigger_channel, trigger_slope, trigge
 	TextMid(250, textHeight, strToPrint, SerifTypeface, textSize);
 	textHeight += textOffset;
 	sprintf(strToPrint, "Trigger Level = %d", trigger_level);
-	TextMid(250, textHeight, strToPrint, SerifTypeface, textSize);	
+	TextMid(250, textHeight, strToPrint, SerifTypeface, textSize);
 	textHeight += textOffset;
 	sprintf(strToPrint, "Mode = %d", mode);
 	TextMid(250, textHeight, strToPrint, SerifTypeface, textSize);
 	textHeight += textOffset;
 	sprintf(strToPrint, "# Channels = %d", nchannels);
-	TextMid(250, textHeight, strToPrint, SerifTypeface, textSize);			
-	
+	TextMid(250, textHeight, strToPrint, SerifTypeface, textSize);
+
 	Stroke(255, 0, 0, 1); // Red line for graph
-	
+
 	return 0;
-	
+
 }
 
-int detectCommand(char* command) {	
-	
+int detectCommand(char* command) {
+
 	if (strstr(command, "nchannels") != NULL) {
 		if (strstr(command, "1") != NULL) {
 			nchannels = 1;
@@ -220,7 +220,7 @@ int detectCommand(char* command) {
 			printf("[ERROR] Invalid nchannels input. Please use either 1 or 2.\n");
 		}
 		return 1;
-		
+
 	} else if (strstr(command, "mode") != NULL) {
 		if (strstr(command, "free_run") != NULL) {
 			mode = 1;
@@ -230,12 +230,12 @@ int detectCommand(char* command) {
 			printf("Set mode to trigger\n");
 		} else {
 			printf("[ERROR] Invalid mode input. Please use either free_run or trigger.\n");
-		}	
+		}
 		return 2;
-		
+
 	} else if (strstr(command, "trigger_level") != NULL) {
 		return 3;
-		
+
 	} else if (strstr(command, "trigger_slope") != NULL) {
 		if (strstr(command, "positive") != NULL) {
 			printf("Set trigger slope to positive\n");
@@ -245,7 +245,7 @@ int detectCommand(char* command) {
 			printf("[ERROR] Invalid trigger slope input. Please use either positive or negative.\n");
 		}
 		return 4;
-		
+
 	} else if (strstr(command, "trigger_channel") != NULL) {
 		if (strstr(command, "1") != NULL) {
 			printf("Set trigger channel to 1\n");
@@ -257,7 +257,7 @@ int detectCommand(char* command) {
 			printf("[ERROR] Invalid trigger channel input. Please use either positive or negative.\n");
 		}
 		return 5;
-		
+
 	} else if (strstr(command, "yscale") != NULL) {
 		if (strstr(command, "0.5") != NULL) {
 			printf("Set yscale to 0.5\n");
@@ -275,7 +275,7 @@ int detectCommand(char* command) {
 			printf("[ERROR] Invalid yscale input.\n");
 		}
 		return 6;
-		
+
 	} else if (strstr(command, "xscale") != NULL) {
 		if (strstr(command, "10000") != NULL) {
 			printf("Set xscale to 10000\n");
@@ -303,12 +303,12 @@ int detectCommand(char* command) {
 			xscale = 1;
 		} else {
 			printf("[ERROR] Invalid xscale input.\n");
-		}			
-		return 7;		
-		
+		}
+		return 7;
+
 	} else if (strstr(command, "start") != NULL) {
 		return 8;
-			
+
 	} else {
 		printf("[ERROR] Command Not Recognized. Please try again.\n");
 		return 0;
